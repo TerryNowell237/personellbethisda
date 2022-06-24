ffa62807be30eda45a108975ae15c8b94b01edba
unknown <raphaondbo@gmail.com>
2022-06-24 12:22:45 +0100

    Adding phone code coque

---
+++ Telephone/Assets/JS/telephone.js
@@ -0,0 +1,8 @@
+function allumer(){
+    screen = document.getElementById('screen');
+    if(screen.style.background == "white"){
+        screen.style.background = "black";
+    }else{
+        screen.style.background ="white";
+    }
+}
